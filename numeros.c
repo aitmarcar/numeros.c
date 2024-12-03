6d2eeb2901621b81f59fa2df0a6e42895d51d7fa
aitmarcar <aitanamc29@gmail.com>
2024-12-03 13:43:27 +0100

    Add files via upload

---
+++ numeros.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main(){
+    int n;
+
+    do{
+    printf("Introduix un número: ");
+    scanf("%d",&n);
+    }while();
+
+    if(n == 1) printf("U\n");
+    if(n == 2) printf("Dos\n");
+    if(n == 3) printf("Tres\n");
+    if(n == 4) printf("Quatre\n");
+    if(n == 5) printf("Cinc\n");
+    return;
+}
